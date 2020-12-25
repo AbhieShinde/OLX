1acc3abe7bf09ab3500d9edb8aef0b199734851b
ashinde <shindea890@gmail.com>
2020-12-25 13:16:04 +0530

    Updating show(), Added rekeyArray()

--- src/custom_functions.php
+++ src/custom_functions.php
@@ -2,9 +2,25 @@
 /**
  * PHP Common Funtions
  */
-function show( $mixValue ) {
-	echo '<pre style="background-color:white; color:rgb(32, 56, 18);padding:5px; border: 1px solid black; border-radius: 4px;">', htmlentities( print_r( $mixValue, true ) ), '</pre>';
-	echo '<pre style="background-color:white; color:rgb(32, 56, 18);padding:5px; border: 1px solid black; border-radius: 4px;">', htmlentities( print_r( get_class_methods( $mixValue ), true ) ), '</pre>';
+
+function validArray( $arrmixValues, $intCount = 1, $boolCheckForEquality = false ) {
+	$boolIsValid = is_array( $arrmixValues ) ? ( $intCount <= count( $arrmixValues ) ) ? true : false : false;
+	if( $boolCheckForEquality && $boolIsValid ) {
+		$boolIsValid = ( $intCount == count( $arrmixValues ) ) ? true : false;
+	}
+
+	return $boolIsValid;
+}
+
+function show( $mixValue, $boolShowOnlyMethods = false ) {
+	if ( false == $boolShowOnlyMethods ) {
+		echo '<pre style="background-color:white; color:rgb(32, 56, 18);padding:5px; border: 1px solid black; border-radius: 4px;">', htmlentities( print_r( $mixValue, true ) ), '</pre>';
+	}
+	if ( validArray( get_class_methods( $mixValue ) ) ) {
+		echo '<pre style="background-color:white; color:rgb(32, 56, 18);padding:5px; border: 1px solid black; border-radius: 4px;">', htmlentities( print_r( get_class_methods( $mixValue ), true ) ), '</pre>';
+	} elseif( true == $boolShowOnlyMethods ) {
+		echo '<pre style="background-color:white; color:rgb(32, 56, 18);padding:5px; border: 1px solid black; border-radius: 4px;">', htmlentities( 'No Class Methods Present' ), '</pre>';
+	}
     exit;
 }
 
@@ -15,15 +31,6 @@ function validInteger( $intNumber ) {
 function validString( $strString ) {
     $strString = ( false == is_array( $strString ) ) ? trim( ( string ) $strString ) : NULL;
     return is_string( $strString ) ? ( '' == $strString || ' ' == $strString ) ? false : true : false;
-}
-
-function validArray( $arrmixValues, $intCount = 1, $boolCheckForEquality = false ) {
-	$boolIsValid = is_array( $arrmixValues ) ? ( $intCount <= count( $arrmixValues ) ) ? true : false : false;
-	if( $boolCheckForEquality && $boolIsValid ) {
-		$boolIsValid = ( $intCount == count( $arrmixValues ) ) ? true : false;
-	}
-
-	return $boolIsValid;
 }
 
 function validIntegerArray( $arrmixInputArray ) : bool {
@@ -62,6 +69,42 @@ function is_dir_empty( $resDirectory ) {
 
 	closedir( $resDirectory );
 	return true;
+}
+
+function rekeyArray( $strKeyFieldName, $arrmixUnkeyedData, $boolMakeKeyLowerCase = false, $boolHasMultipleArraysWithSameKey = false, $boolExcludeNulls = false ) {
+	if( false == validArray( $arrmixUnkeyedData ) ) {
+		return $arrmixUnkeyedData;
+	}
+
+	$arrmixRekeyedData = [];
+
+	if( 'index' != $strKeyFieldName ) {
+		foreach( $arrmixUnkeyedData as $arrmixUnkeyedData ) {
+			if( true == $boolExcludeNulls && true == is_null( $arrmixUnkeyedData[$strKeyFieldName] ) ) {
+				continue;
+			}
+			if( true == $boolHasMultipleArraysWithSameKey ) {
+				$strKey                       = ( true == $boolMakeKeyLowerCase ) ? strtolower( trim( $arrmixUnkeyedData[$strKeyFieldName] ) ) : trim( $arrmixUnkeyedData[$strKeyFieldName] );
+				$arrmixRekeyedData[$strKey][] = $arrmixUnkeyedData;
+			} else {
+
+				if( false == isset ( $arrmixUnkeyedData[$strKeyFieldName] ) ) {
+					$strKey = '';
+				} else {
+					$strKey = ( true == $boolMakeKeyLowerCase ) ? strtolower( trim( $arrmixUnkeyedData[$strKeyFieldName] ) ) : trim( $arrmixUnkeyedData[$strKeyFieldName] );
+				}
+
+				$arrmixRekeyedData[$strKey] = $arrmixUnkeyedData;
+			}
+		}
+	} else {
+
+		foreach( $arrmixUnkeyedData as $arrmixUnkeyedData ) {
+			$arrmixRekeyedData[] = $arrmixUnkeyedData;
+		}
+	}
+
+	return $arrmixRekeyedData;
 }
 
 ?>
